30d95975ef9fbd3cbaa540dfe9f235d0df94b670
Shashank1487 <shank1487@gmail.com>
2018-10-10 10:47:29 +0530

    Updating the server.js file

--- server/server.js
+++ server/server.js
@@ -4,35 +4,57 @@ mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/TodoApp");
 
 let Todo = mongoose.model("Todo", {
-  text: { type: String },
-  completed: { type: Boolean },
-  completedAt: { type: Number }
+  text: { type: String, required: true, minLength: 1, trim: true },
+  completed: { type: Boolean, default: false },
+  completedAt: { type: Number, default: null }
 });
 
-let newTodo = new Todo({
-  text: "cook dinner"
-});
+// let newTodo = new Todo({
+//   text: "cook lunch"
+// });
 
 // newTodo.save().then(
 //   doc => {
 //     console.log("Inserted todo: ", doc);
 //   },
 //   err => {
-//     console.log("unable to insert todo");
+//     console.log("unable to insert todo: ", err);
 //   }
 // );
 
-let watchUfcTodo = new Todo({
-  text: "watch ufc",
-  completed: true,
-  completedAt: 200
+// let watchUfcTodo = new Todo({
+//   text: "watch ufc",
+//   completed: true,
+//   completedAt: 200
+// });
+
+// watchUfcTodo.save().then(
+//   doc => {
+//     console.log("Inserted todo: ", JSON.stringify(doc, undefined, 2));
+//   },
+//   err => {
+//     console.log("Unable to insert todo", e);
+//   }
+// );
+
+let User = mongoose.model("User", {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 1
+  }
 });
 
-watchUfcTodo.save().then(
+let user = new User({
+  email: "shashank.kumar@gmail.com"
+});
+
+user.save().then(
   doc => {
-    console.log("Inserted todo: ", JSON.stringify(doc, undefined, 2));
+    console.log("User saved successfully ", JSON.stringify(doc, undefined, 2));
   },
   err => {
-    console.log("Unable to insert todo", e);
+    console.log("Unable to save user ", err);
   }
 );
