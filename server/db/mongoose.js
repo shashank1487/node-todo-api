435a92dbfca1e7ae086220363f6e6ea58c771019
Shashank1487 <shank1487@gmail.com>
2018-10-11 09:59:16 +0530

    Updated the mongo db user password

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://shashank:M8$tek14!$@ds021994.mlab.com:21994/user");
+mongoose.connect("mongodb://shashank:mongo1487@ds021994.mlab.com:21994/user");
 
 module.exports = { mongoose };
