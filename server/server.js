85a8d5be58d0e1a5c92a340b9f34223c8cdedbdb
Shashank1487 <shank1487@gmail.com>
2018-10-11 09:07:36 +0530

    Added router for getting individual item

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+let { ObjectID } = require("mongodb");
 let express = require("express");
 let bodyParser = require("body-parser");
 
@@ -31,6 +32,25 @@ app.get("/todos", (req, res) => {
     },
     err => res.status(400).send(err)
   );
+});
+
+app.get("/todos/:id", (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 app.listen(3000, () => {
